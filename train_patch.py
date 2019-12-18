8b4280022c6b6788e6d5e4f35a7056fe937e36a2
donnjonn <44286455+donnjonn@users.noreply.github.com>
2019-12-18 09:27:18 +0100

    Update train_patch.py

--- train_patch.py
+++ train_patch.py
@@ -13,12 +13,16 @@ sys.path.append('/home/jonas/deep-person-reid/adversarial_yolo')
 from adversarial_yolo.train_patch import PatchTrainer
 from torchvision import transforms
 import torch.nn.functional as F
+import pandas as pd
+
+
 pt = PatchTrainer("paper_obj")
 cfg = get_default_config()
 adv_patch = pt.generate_patch("gray")
 adv_patch.requires_grad_(True)
 dm = build_datamanager()
 cfg.use_gpu = True
+cfg.test.evaluate = True
 model = torchreid.models.build_model(
         name=cfg.model.name,
         num_classes=dm.num_train_pids,
@@ -26,17 +30,34 @@ model = torchreid.models.build_model(
         pretrained=cfg.model.pretrained,
         use_gpu=cfg.use_gpu
     )
+centers_df = pd.read_csv('featuretensors/centers.csv')
+centers_df.drop(centers_df.columns[[0]], axis=1, inplace=True)
+target = centers_df.values[0]
 optimizer = torch.optim.Adam([adv_patch], lr=learning_rate, amsgrad=True)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=50)
-print("scheduler ok")
 img_size = 416
 batch_size = 8
 n_epochs = 100
 max_lab = 14
 trainloader, testloader = dm.return_dataloaders()
+galleryloader = testloader['market1501']['gallery']
+targets = list(testloader.keys())
 epoch_length = len(trainloader)
 counter = 0
 engine = build_engine(cfg, dm, model, optimizer, scheduler)
+
+def tester(qf, q_pids, q_camids, batch_idx, data):
+    imgs, pids, camids = engine._parse_data_for_eval(data)
+    #imgs = imgs.cuda()
+    features = engine._extract_features(imgs)
+    features = features.data.cpu()
+    #print("features lijst: ", features)
+    #print("lengte:", len(features))
+    qf.append(features)
+    q_pids.extend(pids)
+    q_camids.extend(camids)
+    return features
+
 for epoch in range(n_epochs):
             print(counter)
             counter+=1
@@ -44,15 +65,19 @@ for epoch in range(n_epochs):
             ep_nps_loss = 0
             ep_tv_loss = 0
             ep_loss = 0
-            for batch_idx, data in enumerate(trainloader):
-                imgs, pids = engine._parse_data_for_train(data)
+            qf, q_pids, q_camids = [], [], []
+            for batch_idx, data in enumerate(galleryloader):
+                imgs, pids, camids = engine._parse_data_for_eval(data)
                 img_batch = imgs.cuda()
-                #img = img_batch[1, :, :,:]
+                img = img_batch[0, :, :,:]
+                print(img)
+                #transform = transforms.Compose([transforms.Normalize((-1,-1,-1),(2,2,2))])
+                #img = transform(img)
                 #print(img)
-                #imgplt = transforms.ToPILImage()(img.detach().cpu())
+                imgplt = transforms.ToPILImage()(img.detach().cpu())
                 #imgpltmat = np.array(imgplt)
                 #print(imgpltmat)
-                #print(pids[0])
+                print(pids[0])
                 #plt.imshow(imgplt)
                 #plt.show()
                 pid_batch = pids.cuda()
@@ -66,11 +91,33 @@ for epoch in range(n_epochs):
                 p_img_batch = F.interpolate(p_img_batch, (256, 128))
                 img = p_img_batch[1, :, :,]
                 imgplt = transforms.ToPILImage()(img.detach().cpu())
-                imgplt.show()
+		
+                plt.imshow(imgplt)
+                plt.show()
+                features = tester(qf, q_pids, q_camids, batch_idx, data)
+                #print(features.size())
+                afst_array = []
+                for row in features:
+                    afst_array.append(torch.dist(torch.from_numpy(target), row.double()))
+                    #print("afst: ",afst)
+                afst_array = torch.stack(afst_array)
+                #torch.cat(afst_array, out=b)
+                afst = torch.mean(afst_array)
+                afst.requires_grad_(True)
+                #batch_size = img_batch.size()
+                #afst = afst.item()/batch_size[0]
+                print(afst)
+                #print(qf)
+                afst.backward()
+                optimizer.step()
+                optimizer.zero_grad()
                 #img.permute(1,2,0)
                 #imgplt = transforms.ToPILImage()(img)
                 #plt.imshow(imgplt)
                 #plt.show()
                 #img.show()
                 continue
+
+
+
 
